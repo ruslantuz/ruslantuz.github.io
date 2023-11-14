aff361305fd15e2a60f51eb32e34e7634720c587
ruslantuz <ruslantuz10@gmail.com>
2023-11-14 22:19:23 +0200

    Added pr4

--- lab_js/pr_4/task_2/script.js
+++ lab_js/pr_4/task_2/script.js
@@ -1,21 +1,149 @@
+document.addEventListener('DOMContentLoaded', getMyLocation);
+let watchId = null;
 
-function generate(){
-    var rtl = document.getElementById('rtl').value;
-    var rtr = document.getElementById('rtr').value;
-    var rbr = document.getElementById('rbr').value;
-    var rbl = document.getElementById('rbl').value;
 
-    var ttl = document.getElementById('ttl');
-    var ttr = document.getElementById('ttr');
-    var tbr = document.getElementById('tbr');
-    var tbl = document.getElementById('tbl');
+let ourCoords = {
+    latitude: 48.94321,
+    longitude: 24.73380
+};  
+let previousCoords = null;
+let map;
+let markers = []; 
 
-    var block = document.getElementById('block');
+function getMyLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+        var watchButton = document.getElementById('watch');
+        watchButton.onclick = watchLocation;
+        var clearWatchButton = document.getElementById('clearWatch');
+        clearWatchButton.onclick = clearWatch;
 
-    ttl.value = rtl;
-    ttr.value = rtr;
-    tbr.value = rbr;
-    tbl.value = rbl;
-    
-    block.style.borderRadius = rtl + "px " + rtr + "px " + rbr + "px " + rbl + "px";
+        document.getElementById('destinationLat').value = ourCoords.latitude;
+        document.getElementById('destinationLng').value = ourCoords.longitude;
+
+        document.getElementById('setDestination').addEventListener('click', setDestination);
+
+        document.getElementById('scrollToDestination').addEventListener('click', scrollToDestination);
+    }
+    else{
+        alert ("Oops, no geolocation support");
+    }
+}
+
+function displayLocation(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    if (!previousCoords || previousCoords.latitude !== latitude || previousCoords.longitude !== longitude) {
+        let div = document.getElementById("location");
+        div.innerHTML = `You are at Latitude: ${latitude}, Longitude: ${longitude}`;
+        div.innerHTML += `(with ${position.coords.accuracy} meters accuracy)`;
+
+        updateMap(latitude, longitude);
+
+        let km = computeDistance(position.coords, ourCoords);
+        let distance = document.getElementById("distance");
+        distance.innerHTML = `You are ${km.toFixed(2)} km from the College`;
+
+        addMarkerWithInfo(latitude, longitude, new Date(position.timestamp));
+
+        previousCoords = {
+            latitude: latitude,
+            longitude: longitude
+        };
+    }
+}
+
+function displayError(error){
+    const errorTypes = {
+        0: "Unknown error",
+        1: "Permission denied by user",
+        2: "Position is not available",
+        3: "Request timed out"
+    }
+    let errorMessage = errorTypes[error.code];
+    if (error.code == 0 || error.code == 2){
+        errorMessage = errorMessage + " " + error.message;
+    }
+    let div = document.getElementById("location");
+    div.innerHTML = errorMessage;
+}
+
+
+
+function updateMap(latitude, longitude) {
+    if (!map) {
+        map = L.map('map').setView([latitude, longitude], 15);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+    } 
+    L.marker([latitude, longitude]).addTo(map);
+}
+
+
+function addMarkerWithInfo(latitude, longitude, timestamp) {
+    const popupContent = `Latitude: ${latitude}<br>Longitude: ${longitude}<br>Time: ${timestamp.toLocaleString("uk-UK")}`;
+    const marker = L.marker([latitude, longitude]).bindPopup(popupContent);
+
+    marker.addTo(map);
+    markers.push(marker);
+}
+
+function setDestination() {
+    const destinationLat = parseFloat(document.getElementById('destinationLat').value);
+    const destinationLng = parseFloat(document.getElementById('destinationLng').value);
+
+    ourCoords = {
+        latitude: destinationLat,
+        longitude: destinationLng
+    };
+
+    updateMap(destinationLat, destinationLng);
+
+    clearMarkers();
+    addMarkerWithInfo(destinationLat, destinationLng, new Date());
+}
+
+function scrollToDestination() {
+    const destinationLat = parseFloat(document.getElementById('destinationLat').value);
+    const destinationLng = parseFloat(document.getElementById('destinationLng').value);
+
+    map.setView([destinationLat, destinationLng], 15);
+}
+
+function clearMarkers() {
+    markers.forEach(marker => marker.remove());
+    markers = [];
+}
+
+
+
+
+function computeDistance(startCoords, destCoords){
+    let startLatRads = degreesToRadians(startCoords.latitude);
+    let startLongRads = degreesToRadians(startCoords.longitude);
+    let destLatRads = degreesToRadians(destCoords.latitude);
+    let destLongRads = degreesToRadians(destCoords.longitude);
+    let Radius = 6371; // вказаний радіус землі у км
+    let distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + Math.cos(startLatRads) * Math.cos(destLatRads) *
+    Math.cos(startLongRads - destLongRads)) * Radius;
+    return distance;
+}
+function degreesToRadians(degrees) {
+    let radians = (degrees * Math.PI)/180;
+    return radians;
+}
+
+
+function watchLocation(){
+    watchId = navigator.geolocation.watchPosition(displayLocation,displayError);    
+}
+
+function clearWatch(){
+    if(watchId){
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+    }
 }
