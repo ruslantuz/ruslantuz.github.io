bbc85b443bb0ad07b1e466e25294d3d31edd4608
ruslantuz <ruslantuz10@gmail.com>
2023-11-28 22:19:54 +0200

    files and link change

---
+++ lab_js/lr_5/cart/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    displayCartItems();
+});
+
+function displayCartItems() {
+    var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    var cartTableBody = document.getElementById("cartTableBody");
+
+    cartTableBody.innerHTML = "";
+
+    cartItems.forEach(function (item, index) {
+        var row = document.createElement("tr");
+        var productName = item.name;
+        var total = item.quantity * item.price;
+
+        row.innerHTML = "<td>" + (index + 1) + "</td><td><a href='" + item.link + "'>" + productName + "</a></td><td>" + item.price + " грн</td><td>" + item.quantity + "</td><td>" + total + " грн</td><td><button class='delete-btn' onclick='deleteItem(" + index + ")'>Видалити</button></td>";
+        cartTableBody.appendChild(row);
+    });
+}
+
+function checkout() {
+    alert("Оплата успішно виконана!");
+
+    localStorage.removeItem("cartItems");
+    displayCartItems(); 
+}
+
+function deleteItem(index) {
+    var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    cartItems.splice(index, 1);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    displayCartItems();
+}
