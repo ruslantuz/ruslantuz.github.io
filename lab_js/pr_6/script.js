9f4dd6b72c8c30f1dcc06fb08670789317f84594
ruslantuz <ruslantuz10@gmail.com>
2023-12-05 20:07:57 +0200

    json change for test

---
+++ lab_js/pr_6/script.js
@@ -0,0 +1,66 @@
+const header = document.querySelector('header')
+const section = document.querySelector('section')
+
+const requestURL = 'https://ruslantuz.github.io/lab_js/pr_6/json/example.json';
+
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+
+request.send();
+
+request.onload = function(){
+    const superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+    console.log(superHeroes)
+}
+
+function populateHeader(superHeroes){
+    const squadName = document.createElement('h1');
+    const squadinfo = document.createElement('p');
+
+    squadName.textContent = superHeroes.squadName
+    squadinfo.textContent = "Hometown: " + superHeroes.homeTown + " // Formed: " + superHeroes.formed;
+    
+    header.appendChild(squadName);
+    header.appendChild(squadinfo);
+}
+function showHeroes(superHeroes){
+    superHeroes.members.forEach(
+        (member) => {
+            const article = document.createElement('article');
+
+            const memberName = document.createElement('h2')
+            memberName.textContent = member.name;
+            
+            const secretIdentity = document.createElement('p');
+            secretIdentity.textContent = "Secret Identity: " + member.secretIdentity;
+            
+            const age = document.createElement('p');
+            age.textContent = "Age: " + member.age;
+            
+            const superPowersHead = document.createElement('p')
+            superPowersHead.textContent = "Superpowers:";
+            
+            const powerList = document.createElement('ul')
+
+            member.powers.forEach((power) => {
+                const powerInfo = document.createElement('li');
+                powerInfo.textContent = power
+                powerList.appendChild(powerInfo)
+            })
+
+
+            article.appendChild(memberName);
+            article.appendChild(secretIdentity);
+            article.appendChild(age);
+            article.appendChild(superPowersHead);
+            article.appendChild(powerList);
+
+            section.appendChild(article);
+        }); 
+
+
+
+}
